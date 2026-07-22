866f8286991fe6f1e0aac6189c4317db69590430
isaactoh <isaactoh29@gmail.com>
2026-07-22 10:16:52 +0800

    Initial commit

---
+++ src/main/java/com/isaactoh/racefinder/RacefinderApplication.java
@@ -0,0 +1,13 @@
+package com.isaactoh.racefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacefinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacefinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isaactoh/racefinder/RacefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaactoh.racefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacefinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
